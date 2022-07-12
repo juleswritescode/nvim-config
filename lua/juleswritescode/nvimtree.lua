a07d2c86efd83d24f5ddbf5b2d439059d6d53bea
Julian <juliandomke@outlook.de>
2022-07-13 00:43:33 +0200

    nvim tree fixes

--- lua/juleswritescode/nvimtree.lua
+++ lua/juleswritescode/nvimtree.lua
@@ -1,5 +1,3 @@
--- following options are the default
--- each of these are documented in `:help nvim-tree.OPTION_NAME`
 vim.g.nvim_tree_icons = {
   default = "",
   symlink = "",
@@ -21,17 +19,58 @@ vim.g.nvim_tree_icons = {
   },
 }
 
+local mappings = {
+  { key = { "l", "<CR>", "o", "<2-LeftMouse>" }, action = "edit" },
+  { key = "<C-e>", action = "edit_in_place" },
+  { key = "O", action = "edit_no_picker" },
+  { key = { "<C-]>", "<2-RightMouse>" }, action = "cd" },
+  { key = "v", action = "vsplit" },
+  { key = "<C-x>", action = "split" },
+  { key = "<C-t>", action = "tabnew" },
+  { key = "<", action = "prev_sibling" },
+  { key = ">", action = "next_sibling" },
+  { key = "P", action = "parent_node" },
+  { key = "h", action = "close_node" },
+  { key = "<Tab>", action = "preview" },
+  { key = "K", action = "first_sibling" },
+  { key = "J", action = "last_sibling" },
+  { key = "I", action = "toggle_git_ignored" },
+  { key = "H", action = "toggle_dotfiles" },
+  { key = "U", action = "toggle_custom" },
+  { key = "R", action = "refresh" },
+  { key = "a", action = "create" },
+  { key = "d", action = "remove" },
+  { key = "D", action = "trash" },
+  { key = "r", action = "rename" },
+  { key = "<C-r>", action = "full_rename" },
+  { key = "x", action = "cut" },
+  { key = "c", action = "copy" },
+  { key = "p", action = "paste" },
+  { key = "y", action = "copy_name" },
+  { key = "Y", action = "copy_path" },
+  { key = "gy", action = "copy_absolute_path" },
+  { key = "[e", action = "prev_diag_item" },
+  { key = "[c", action = "prev_git_item" },
+  { key = "]e", action = "next_diag_item" },
+  { key = "]c", action = "next_git_item" },
+  { key = "-", action = "dir_up" },
+  { key = "s", action = "system_open" },
+  { key = "f", action = "live_filter" },
+  { key = "F", action = "clear_live_filter" },
+  { key = "q", action = "close" },
+  { key = "W", action = "collapse_all" },
+  { key = "E", action = "expand_all" },
+  { key = "S", action = "search_node" },
+  { key = ".", action = "run_file_command" },
+  { key = "<C-k>", action = "toggle_file_info" },
+  { key = "g?", action = "toggle_help" },
+  { key = "m", action = "toggle_mark" },
+}
+
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
   return
 end
-
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
-  return
-end
-
-local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvim_tree.setup {
   disable_netrw = true,
@@ -41,6 +80,11 @@ nvim_tree.setup {
     "startify",
     "dashboard",
     "alpha",
+  },
+  renderer = {
+    icons = {
+      git_placement = "after",
+    }
   },
   auto_close = true,
   open_on_tab = false,
@@ -85,11 +129,7 @@ nvim_tree.setup {
     auto_resize = true,
     mappings = {
       custom_only = false,
-      list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
-        { key = "v", cb = tree_cb "vsplit" },
-      },
+      list = mappings
     },
     number = false,
     relativenumber = false,
@@ -99,7 +139,6 @@ nvim_tree.setup {
     require_confirm = true,
   },
   quit_on_open = 0,
-  git_hl = 1,
   disable_window_picker = 0,
   root_folder_modifier = ":t",
   show_icons = {
